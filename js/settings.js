123b8d8b9acd43bfeb78ea5befccabba5f1b9bdb
xanindustries01-code <xanindustries01@gmail.com>
2026-06-12 19:12:07 -0400

    Change SETTINGS_SHUTDOWN_URL to about:blank

--- js/settings.js
+++ js/settings.js
@@ -5,6 +5,6 @@ const SETTINGS_ICONS_SMALL_PATH = "img/ui/icons-small/";
 /* Duration of the openWindow transfer animation */
 const SETTINGS_TRANSFER_ANIMATION_DURATION = 300;
 
-const SETTINGS_SHUTDOWN_URL = "https://www.apple.com/";
+const SETTINGS_SHUTDOWN_URL = "about:blank";
 
 const SETTINGS_WHERE_ROOT = "Macintosh HD:";
